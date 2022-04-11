9955fd2b3ad40a0d4ca1a7ddc0169dde82253ef1
loganzimm4 <101672520+loganzimm4@users.noreply.github.com>
2022-04-11 10:58:34 -0400

    commit tree 469530830d0ce260002101cfba761e181239e636

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+//Remove Main Element
+const main = document.getElementById("main");
+main.remove();
+
+// Add New h1 tag with Id 'victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Logan is the champion';
+document.body.appendChild(newHeader);
